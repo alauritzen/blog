ab9dd35e637492e9493ef845d794ee48f8271685
Alan <alaurit@gmail.com>
2016-06-08 12:38:35 -0500

    added navbar section

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -5,13 +5,14 @@
     <meta charset="UTF-8">
     <!-- Add meta below per W3 schools, to ensure proper rendering and touch zooming; www.w3schools.com/bootstrap/bootstrap_get_started.asp -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Compiled and minified Materialize CSS -->
-    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css"> -->
-    <!-- Latest compiled and minified Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    
+    <link rel="stylesheet" href="/css/blog_css.css">
     @yield('top-script')
 </head>
 <body>
+    @include('partials.navbar')
+    
     @if (Session::has('successMessage'))
         <div class='alert alert-success'>{{{ Session::get('successMessage') }}}</div>
     @endif
@@ -19,14 +20,16 @@
         <div class='alert alert-danger'>{{{ Session::get('errorMessage') }}}</div>
     @endif
 
-    @yield('content')
+    <div class="container">
+        <div class="row">
+            @yield('content')
+        </div>
+    </div>
     
-<!-- jQuery snippet -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
-<!-- Compiled and minified Materialize JavaScript -->
-<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script> -->
+    <!-- jQuery snippet -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
 
-<!-- Latest compiled and minified Bootstrap JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    <!-- Latest compiled and minified Bootstrap JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 </body>
 </html>
