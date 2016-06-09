50048d1b4905aeb96082a4ce424fcead71925e61
Alan <alaurit@gmail.com>
2016-06-09 10:08:51 -0500

    added some styling to index page and navbar

--- app/views/posts/index.blade.php
+++ app/views/posts/index.blade.php
@@ -1,12 +1,13 @@
 @extends('layouts.master')
 
 @section('content')
+    <div class='post-index'>
+        @foreach($posts as $post)
+            <a href={{ action('PostsController@show', $post->id) }}><h2> {{{ $post->title }}}</h2> </a>
+            <p> {{{ $post->description }}} </p>
+        @endforeach
 
-    @foreach($posts as $post)
-        <a href={{ action('PostsController@show', $post->id) }}><h2> {{{ $post->title }}}</h2> </a>
-        <p> {{{ $post->description }}} </p>
-    @endforeach
-
-    {{ $posts->links() }}
+        {{ $posts->links() }}
+    </div>
 
 @stop
