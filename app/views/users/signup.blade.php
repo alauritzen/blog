43fd376592c408dd6b80c5177552a084a43186d2
Alan <alaurit@gmail.com>
2016-06-09 14:33:32 -0500

    fixed alignment issue on forms

--- app/views/users/signup.blade.php
+++ app/views/users/signup.blade.php
@@ -10,31 +10,23 @@
 {{ Form::open(array('action'=>'UsersController@createUser')) }}
     
     <p class="form-input signup-form">
-        {{ Form::label('username', 'Username') }}
-        <span class="inline-form">
-            {{ Form::text('username', null, array('value'=>Input::old('username'), 'class'=>'input_box')) }}
-        </span>
+    {{ Form::label('username', 'Username') }}
+    {{ Form::text('username', null, array('value'=>Input::old('username'), 'class'=>'input_box')) }}
     </p>
 
     <p class="form-input signup-form">
-        {{ Form::label('email', 'Email') }}
-        <span class="inline-form">
-            {{ Form::email('email', null, array ('value'=>Input::old('title'), 'class'=>'input_box')) }}
-        </span>
+    {{ Form::label('email', 'Email') }}
+    {{ Form::email('email', null, array ('value'=>Input::old('title'), 'class'=>'input_box')) }}
     </p>
 
     <p class="form-input signup-form">
-        {{ Form::label('password', 'Password') }}
-        <span class="inline-form">
-            {{ Form::password('password') }}
-        </span>
+    {{ Form::label('password', 'Password') }}
+    {{ Form::password('password') }}
     </p>
 
     <p class="form-input signup-form">
-        {{ Form::label('passwordConfirm', 'Confirm password') }}
-        <span class="inline-form">
-            {{ Form::password('passwordConfirm') }}
-        </span>
+    {{ Form::label('passwordConfirm', 'Confirm password') }}
+    {{ Form::password('passwordConfirm') }}
     </p>
 
     <p>
