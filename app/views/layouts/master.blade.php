be796bec5955a5ddd3ca617ebb135eb868cef228
Alan <alaurit@gmail.com>
2016-06-09 15:31:27 -0500

    added footer with style

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -8,6 +8,10 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     
     <link rel="stylesheet" href="/css/blog_css.css">
+
+    <!-- Font Awesome stylesheet -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
+    
     @yield('top-script')
 </head>
 <body>
@@ -26,6 +30,7 @@
         </div>
     </div>
     
+    @include('partials.footer')
     <!-- jQuery snippet -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
 
---
+++ app/views/partials/footer.blade.php
@@ -0,0 +1,39 @@
+<!-- <nav class="navbar navbar-static-bottom">
+    <div class="container-fluid" id="footer">
+        <ul class="list-unstyled contact_info pull-right">
+            <li><h1>Alan Lauritzen</h1></li>
+            <li><h2>San Antonio, TX</h2></li>
+            <ul class="list-unstyled icons">
+                <li><a href=
+                {{{"mailto:alaurit@gmail.com"}}}
+                ><i class="fa fa-paper-plane" aria-hidden="true"></i></a></li>
+
+                <li><a href="http://github.com/alauritzen" target="_blank"><i class="fa fa-github" aria-hidden="true"></i></a></li>
+
+                <li><a href="https://www.linkedin.com/in/alan-lauritzen-02a078104" target="_blank"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+
+                <li><a href="#" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+            </ul>
+        </ul>
+    </div>
+</nav> -->
+
+<footer class="footer">
+    <div class="container">
+        <ul class="list-unstyled contact_info pull-right">
+            <li><h1>Alan Lauritzen</h1></li>
+            <li><h2>San Antonio, TX</h2></li>
+            <ul class="list-unstyled icons">
+                <li><a href=
+                {{{"mailto:alaurit@gmail.com"}}}
+                ><i class="fa fa-paper-plane" aria-hidden="true"></i></a></li>
+
+                <li><a href="http://github.com/alauritzen" target="_blank"><i class="fa fa-github" aria-hidden="true"></i></a></li>
+
+                <li><a href="https://www.linkedin.com/in/alan-lauritzen-02a078104" target="_blank"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+
+                <li><a href="#" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+            </ul>
+        </ul>
+    </div>
+</footer>
