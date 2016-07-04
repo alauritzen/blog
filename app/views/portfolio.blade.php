0d43c152d4c4b29101c29767085b1ec14d2d53c3
Alan <alaurit@gmail.com>
2016-07-04 11:24:49 -0500

    added flit to portfolio

--- app/views/portfolio.blade.php
+++ app/views/portfolio.blade.php
@@ -11,6 +11,8 @@
 @section('content')
     <h1>Portfolio</h1>
     <ul class="list-unstyled">
+        <li><a href='http://flit.site'><h2>Flit.site</h2></li>
+        <p>A capstone project, completed with two fellow students. Flit helps freelancers keep track of key dates for their projects. Technologies used include HTML, CSS, Javascript, jQuery, PHP/Laravel, Ajax requests, Stripe API, Mailgun API.</p>
         <li><h2>adlister<h2></li>
         <li><h2>National Parks<h2></li>
         <li><a href={{{action('HomeController@showWeather')}}}><h2>Weather map</h2></a></li>
